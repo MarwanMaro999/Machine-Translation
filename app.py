ba223afbd463cb298bd9801e0993aea140a749c6
Marwan Mohamed <135247619+MarwanMaro999@users.noreply.github.com>
2024-12-12 23:20:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # Load model and tokenizer
-model_path = "model"  # Path to the model directory
+model_path = "model"  
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 
@@ -15,14 +15,12 @@ def home():
 @app.route('/translate', methods=['POST'])
 def translate():
     try:
-        # Get input text from request
         data = request.get_json()
         input_text = data.get("text", "")
 
         if not input_text.strip():
             return jsonify({"error": "Input text is empty"}), 400
 
-        # Convert the input text to lowercase
         input_text = input_text.lower()
 
         # Tokenize and translate
